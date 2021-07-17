4e67c1308dc69f26ccdc63089fa9acfaab1ebada
Victor R. Cardoso <labrax@users.noreply.github.com>
2021-07-17 18:36:08 +0100

    record_streams var toggle

--- defines.cpp
+++ defines.cpp
@@ -1,3 +1,5 @@
 #include "defines.h"
 
+unsigned char record_streams = 0; // at the start time does not record
+
 const double aweighting[9] = { -39.4, -26.2, -16.1, -8.6, -3.2, 0.0, 1.2, 1.0, -1.1 };
